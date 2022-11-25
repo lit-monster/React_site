2c264b8d6e10fb80738c84d991a04957b77a19bf
urassh <shuto.1011@icloud.com>
2022-11-26 08:46:03 +0900

    styled-componentsを導入した。

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledHello = styled.h1`
+    color: red;
+  `;
 
 class Header extends React.Component {
   render() {
     return(
       <header>
-        <h1>header</h1>
+        <StyledHello>header</StyledHello>
       </header>
     );
   }
