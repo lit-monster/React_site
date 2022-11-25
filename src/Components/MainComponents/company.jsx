0235cc05195a6474358a179ee39ea15ccd609249
urassh <shuto.1011@icloud.com>
2022-11-26 01:02:24 +0900

    ついでにその他もろもろMainComponentsに追加だけしといた。

---
+++ src/Components/MainComponents/company.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Company extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Company</h1>
+      </div>
+    );
+  }
+}
+
+export default Company;
---
+++ src/Components/MainComponents/productItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductItem extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>ProductItem</h1>
+      </div>
+    );
+  }
+}
+
+export default ProductItem;
