5828f9cf7006bb5d8b367ebfdde0959272150805
urassh <shuto.1011@icloud.com>
2022-11-26 09:48:12 +0900

    class と cssの定義部分変えた。

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,6 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import logo from './Images/logo.svg'
 
+class Header extends React.Component {
+  render() {
+    return(
+      <Wrapper>
+        <h1>
+            <a href="product.html">
+                <img src={logo} alt="Furniture Design" />
+            </a>
+        </h1>
+        <Toggle>
+            <span id="top-bar"></span>
+            <span id="bottom-bar"></span>
+        </Toggle>
+        <Mask>
+            <ul id="menu" class="SlideOut">
+                <li><a href="https://google.com">PRODUCTS</a></li>
+                <li><a href="https://google.com">ABOUT</a></li>
+                <li><a href="https://google.com">COMPANY</a></li>
+                <li><a href="mailto:shuto.1011@icloud.com">CONTACT</a></li>
+            </ul>
+        </Mask>
+    </Wrapper>
+    );
+  }
+}
+
 const Wrapper = styled.header`
     height: 80px;
     width: 100%;
@@ -66,31 +92,5 @@ const Mask = styled.div`
     }
   }
 `;
-
-class Header extends React.Component {
-  render() {
-    return(
-      <Wrapper>
-        <h1>
-            <a href="product.html">
-                <img src={logo} alt="Furniture Design" />
-            </a>
-        </h1>
-        <Toggle>
-            <span id="top-bar"></span>
-            <span id="bottom-bar"></span>
-        </Toggle>
-        <Mask>
-            <ul id="menu" class="SlideOut">
-                <li><a href="https://google.com">PRODUCTS</a></li>
-                <li><a href="https://google.com">ABOUT</a></li>
-                <li><a href="https://google.com">COMPANY</a></li>
-                <li><a href="mailto:shuto.1011@icloud.com">CONTACT</a></li>
-            </ul>
-        </Mask>
-    </Wrapper>
-    );
-  }
-}
 
 export default Header;
