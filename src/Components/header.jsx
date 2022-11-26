0b738a9c30e0bf696949ebac16a193ad03003916
urassh <shuto.1011@icloud.com>
2022-11-26 09:39:21 +0900

    header部分を追加した。

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,16 +1,94 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from './Images/logo.svg'
 
-const StyledHello = styled.h1`
-    color: red;
-  `;
+const Wrapper = styled.header`
+    height: 80px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    z-index: 5;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+    text-decoration: none;
+    padding:0 80px;
+  }
+`;
+
+const Toggle = styled.div`
+  width: 30px;
+  height: 30px;
+  position: relative;
+  z-index: 30;
+  #top-bar{
+    width: 100%;
+    height:2px;
+    position: absolute;
+    top: 10px;
+    transition: all 0.5s;
+    background-color: #242424;
+  }
+  #bottom-bar{
+    width: 100%;
+    height:2px;
+    position: absolute;
+    top: 20px;
+    transition: all 0.5s;
+    background-color: #242424;
+  }
+`;
+
+const Mask = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 20;
+  background-color: #000;
+  opacity: .8;
+  display: none;
+  ul {
+    position: absolute;
+    top: 30px;
+    left: 50px;
+    z-index: 30;
+    li {
+      a {
+        text-decoration: none;
+        color: #fff;
+        margin-top: 50px;
+        font-size: 14px;
+        line-height: 50px;
+      }
+    }
+  }
+`;
 
 class Header extends React.Component {
   render() {
     return(
-      <header>
-        <StyledHello>header</StyledHello>
-      </header>
+      <Wrapper>
+        <h1>
+            <a href="product.html">
+                <img src={logo} alt="Furniture Design" />
+            </a>
+        </h1>
+        <Toggle>
+            <span id="top-bar"></span>
+            <span id="bottom-bar"></span>
+        </Toggle>
+        <Mask>
+            <ul id="menu" class="SlideOut">
+                <li><a href="https://google.com">PRODUCTS</a></li>
+                <li><a href="https://google.com">ABOUT</a></li>
+                <li><a href="https://google.com">COMPANY</a></li>
+                <li><a href="mailto:shuto.1011@icloud.com">CONTACT</a></li>
+            </ul>
+        </Mask>
+    </Wrapper>
     );
   }
 }
