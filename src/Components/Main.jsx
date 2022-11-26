72cdc0d4ebc8692a568fe98d33d35c5f32b35db1
urassh <shuto.1011@icloud.com>
2022-11-26 11:35:48 +0900

    footer部分を作ったどー！

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Products from './MainComponents/products';
+import styled from 'styled-components';
 
 class Main extends React.Component {
   render() {
     return(
       <React.StrictMode>
-        <div>
+        <Wrapper>
           <Products />
-        </div>
+        </Wrapper>
       </React.StrictMode>
     );
   }
 }
+
+const Wrapper = styled.div`
+  margin: 80px 40px;
+  padding: 0px 40px;
+`;
 
 export default Main;
--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 
 class Footer extends React.Component {
   render() {
     return(
       <React.StrictMode>
-        <footer>
-          <h1>Footer</h1>
-        </footer>
+        <Wrapper>
+          <List>
+              <li><a href="https://instagram.com">INSTAGRAM</a></li>
+              <li><a href="https://twitter.com">TWITTER</a></li>                
+              <li><a href="https://facebook.com">FACEBOOK</a></li>
+          </List>
+          <p>© Furniture Design</p>
+        </Wrapper>
       </React.StrictMode>
     );
   }
 }
+
+const Wrapper = styled.footer`
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  padding:0 80px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  li{
+    a {
+        text-decoration: none;
+        padding-right: 20px;
+        color: #333;
+    }
+  }
+`;
 
 export default Footer;
