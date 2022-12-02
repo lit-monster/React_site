96cfe9c73d427a95ad5828cce68360b5e74118ea
urassh <shuto.1011@icloud.com>
2022-12-03 08:34:57 +0900

    Routing実装した。

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Products from './MainComponents/ProductComponent/products';
+import Product from './MainComponents/ProductComponent/productInfo';
+import About from './MainComponents/about';
+import Company from './MainComponents/company';
 import styled from 'styled-components';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 class Main extends React.Component {
   render() {
@@ -8,7 +12,16 @@ class Main extends React.Component {
       <React.StrictMode>
         <Wrapper>
           <TITLE>Products</TITLE>
-          <Products />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path='/' component={Products} />
+              <Route exact path='/product/:id/info' component={Product} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/company' component={Company} />
+            </Switch>
+            <Link to='/'>Back To Top</Link>
+          </BrowserRouter>
+         
         </Wrapper>
       </React.StrictMode>
     );
