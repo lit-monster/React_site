8eb07f19327781cb5e9ade3a97142b955b695f78
urassh <shuto.1011@icloud.com>
2022-12-03 22:50:38 +0900

    aboutページの作成

--- src/Components/MainComponents/about.jsx
+++ src/Components/MainComponents/about.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import {TITLE} from './ProductComponent/products';
 
 class About extends React.Component {
   render() {
     return(
       <React.StrictMode>
-        <div>
-          <h1>About</h1>
-        </div>
+        <Wrapper>
+          <TITLE>About</TITLE>
+          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </Wrapper>
       </React.StrictMode>
     );
   }
 }
+
+const Wrapper = styled.div`
+  width: 600px;
+  p {
+    font-size: 14px;
+    color: #333;
+    line-height: 28px;
+    margin-bottom: 40px;
+  }
+`;
 
 export default About;
